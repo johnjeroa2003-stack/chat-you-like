3ad112be14c89b21698c885c6a82cb32d19a3792
Johnson Jerald <johnjeroa2003@gmail.com>
2026-03-21 01:16:16 +0530

    Added username feature

--- public/script.js
+++ public/script.js
@@ -3,13 +3,24 @@ const socket = io();
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messages = document.getElementById("messages");
+const usernameInput = document.getElementById("username");
+
+let username = "";
+
+// Set username
+usernameInput.addEventListener("change", () => {
+  username = usernameInput.value;
+});
 
 // Send message
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (input.value.trim() !== "") {
-    socket.emit("chat message", input.value);
+  if (input.value.trim() !== "" && username !== "") {
+    socket.emit("chat message", {
+      user: username,
+      text: input.value,
+    });
     input.value = "";
   }
 });
@@ -17,7 +28,7 @@ form.addEventListener("submit", function (e) {
 // Receive message
 socket.on("chat message", function (msg) {
   const item = document.createElement("div");
-  item.textContent = msg;
+  item.innerHTML = `<strong>${msg.user}:</strong> ${msg.text}`;
   messages.appendChild(item);
 
   messages.scrollTop = messages.scrollHeight;
