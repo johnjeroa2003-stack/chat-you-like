3d44fdfb4ce7312e5c2ffd98ee18dc5b636c245d
Johnson Jerald <johnjeroa2003@gmail.com>
2026-03-21 00:51:59 +0530

    Added chat UI

--- server.js
+++ server.js
@@ -1,52 +1,28 @@
 const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const http = require("http");
+const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// VERY IMPORTANT
 app.use(express.static("public"));
 
-let waitingUser = null;
-
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  console.log("User connected");
 
-  // If someone is already waiting, connect them
-  if (waitingUser) {
-    socket.partner = waitingUser;
-    waitingUser.partner = socket;
-
-    socket.emit("chat message", "Connected to stranger!");
-    waitingUser.emit("chat message", "Connected to stranger!");
-
-    waitingUser = null;
-  } else {
-    waitingUser = socket;
-    socket.emit("chat message", "Waiting for a stranger...");
-  }
-
-  // Send message only to partner
   socket.on("chat message", (msg) => {
-    if (socket.partner) {
-      socket.partner.emit("chat message", msg);
-    }
+    io.emit("chat message", msg);
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
-
-    if (socket.partner) {
-      socket.partner.emit("chat message", "Stranger disconnected.");
-      socket.partner.partner = null;
-    }
-
-    if (waitingUser === socket) {
-      waitingUser = null;
-    }
   });
 });
 
 const PORT = process.env.PORT || 3000;
 
-http.listen(PORT, () => {
-  console.log("Server running");
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
