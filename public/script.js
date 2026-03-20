24973e2a5a5c17e0b479a068b2dca5bdb41cab34
Johnson Jerald <johnjeroa2003@gmail.com>
2026-03-21 01:10:07 +0530

    Fixed chat issue

--- public/script.js
+++ public/script.js
@@ -1,5 +1,24 @@
+const socket = io();
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+
+// Send message
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (input.value.trim() !== "") {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
+});
+
+// Receive message
 socket.on("chat message", function (msg) {
   const item = document.createElement("div");
   item.textContent = msg;
-  document.getElementById("messages").appendChild(item);
+  messages.appendChild(item);
+
+  messages.scrollTop = messages.scrollHeight;
 });
