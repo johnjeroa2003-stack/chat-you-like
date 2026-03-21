7bdfe0205dd383cf259419af0eebe2359a476a4d
Johnson Jerald <johnjeroa2003@gmail.com>
2026-03-21 22:34:37 +0530

    fixed deploy error

--- server.js
+++ server.js
@@ -123,10 +123,11 @@ socket.on("receive-private-message", (msg) => {
   addMessage(msg, "stranger");
 });
 // ----------------------
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+const PORT = process.env.PORT || 3000;
 
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 
 // ----------------------
 // PRIVATE ROOM JOIN
