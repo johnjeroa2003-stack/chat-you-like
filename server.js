044498e0b34bc5a18269d315465a841629b0c105
Johnson Jerald <johnjeroa2003@gmail.com>
2026-03-22 23:04:17 +0530

    fixed chat pairing

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
+let users = {}; // username -> socket
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.json());
 app.use(express.static("public"));
 
-let users = {}; // username -> socket
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
+});
 
-function sendOnlineUsers() {
-  io.emit("online-users", Object.keys(users));
-}
-
+// ----------------------
+// 🚀 CONNECTION
+// ----------------------
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  // REGISTER USER
+  // ✅ REGISTER USER
   socket.on("register", (username) => {
     if (!username) return;
 
@@ -25,52 +29,43 @@ io.on("connection", (socket) => {
     users[username] = socket;
 
     console.log(username + " registered");
-    sendOnlineUsers();
+
+    // 🔥 SEND ONLINE USERS TO EVERYONE
+    io.emit("online-users", Object.keys(users));
   });
 
-  // PRIVATE MESSAGE
-  socket.on("private-message", ({ to, msg, id }) => {
+  // ----------------------
+  // 💬 PRIVATE MESSAGE
+  // ----------------------
+  socket.on("private-message", ({ to, msg }) => {
     const target = users[to];
 
     if (target) {
       target.emit("receive-private-message", {
         from: socket.username,
-        msg,
-        id,
+        msg: msg,
       });
     }
   });
 
-  // MESSAGE SEEN ✔✔
-  socket.on("message-seen", ({ to, id }) => {
-    const target = users[to];
-
-    if (target) {
-      target.emit("message-seen", { id });
-    }
-  });
-
-  // TYPING
-  socket.on("typing", (to) => {
-    const target = users[to];
-    if (target) target.emit("typing", socket.username);
-  });
-
-  socket.on("stop-typing", (to) => {
-    const target = users[to];
-    if (target) target.emit("stop-typing");
-  });
-
-  // DISCONNECT
+  // ----------------------
+  // ❌ DISCONNECT
+  // ----------------------
   socket.on("disconnect", () => {
     if (socket.username) {
       delete users[socket.username];
-      sendOnlineUsers();
+
+      // 🔥 UPDATE ONLINE USERS
+      io.emit("online-users", Object.keys(users));
     }
 
     console.log("User disconnected:", socket.id);
   });
 });
 
+// ----------------------
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server running"));
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
